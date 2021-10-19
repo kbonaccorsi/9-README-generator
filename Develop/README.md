5c135c81e4b7cef57d696bbcb1f905324cfb135f
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-10-19 18:10:21 -0500

    questions display, and a new README.md file is made

---
+++ Develop/README.md
@@ -0,0 +1,16 @@
+{
+
+"title": "ZXF",
+
+"description": "dghk",
+
+"installation instructions": "gjl",
+
+"usage": "gjkl",
+
+"license": "ISC",
+
+"contributions": "hlk;",
+
+"test": "vhjk"
+}
--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,58 @@
-const inquirer = require ("inquirer");
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        name:'title',
+        message:"What is the project's title?"
+    },
+    {
+        type:'input',
+        name:'description',
+        message:"What is the project's description?"
+    },
+    {
+        type:'input',
+        name:'installation instructions',
+        message:"What are the installation instructions?"
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:"What is this application used for?"
+    },
+    {
+        type:'list',
+        name:'license',
+        message:"What is the project's license?",
+        choices: ["ISC", "MIT", "IPL"]
+    },
+    {
+        type:'input',
+        name:'contributions',
+        message:"What are the contribution guidelines?"
+    },
+    {
+        type:'input',
+        name:'test',
+        message:"What tests sould be in place?"
+    },
+];
 
+inquirer.prompt(questions)
+    .then((data) => {
+        console.log(data);
+        const fileName = ("README.md");
+        fs.writeFile(fileName, JSON.stringify(data, null, "\n"), (err) => {
+            err ? console.error(err) : console.log("success!")
+        });
+    });
+    
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
